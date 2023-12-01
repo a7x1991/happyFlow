e795a463d682e004e02b1d78d3aad09a35d3de3d
kevinfrischmann <kevin.frischmann6173@stu.mtec.edu>
2023-12-01 13:59:00 -0700

    https://github.com/a7x1991/happyFlow.git

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,382 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+class LoginPage extends Page {
+
+    get inputUsername () {
+        return $('#user-name');
+    }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnSubmit () {
+        return $('[type="submit"]');
+    }
+
+    get shoppingCart () {
+        return $('.shopping_cart_link');
+    }
+   
+    get addToCartBackpack () {
+        return $('[data-test="add-to-cart-sauce-labs-backpack"]');
+    }
+
+    get CheckOut () {
+        return $('#checkout');
+    }
+
+    get BurgerMenu () {
+        return $('#react-burger-menu-btn')
+    }
+
+    get logOut () {
+        return $('#logout_sidebar_link')
+    }
+
+    get allItems () {
+        return $('#inventory_sidebar_link')
+    }
+
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+    
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+    
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+    
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+    
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+    
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async addBackpack() {
+        await expect(this.addToCartBackpack).toBeExisting()
+        await this.addToCartBackpack.click();
+        
+    }
+
+    async clickShoppingCart() {
+        await expect(this.shoppingCart).toBeExisting()
+        await this.shoppingCart.click();
+        
+    }
+
+    async clickCheckOut() {
+        await expect(this.CheckOut).toBeExisting()
+        await this.CheckOut.click();
+    }
+
+    async clickHamburgerMenu() {
+        await expect(this.BurgerMenu).toBeExisting()
+        await this.BurgerMenu.click();
+    }
+
+    async clickLogOut() {
+        await expect(this.logOut).toBeExisting()
+        await this.logOut.click();
+    }
+    
+
+    open () {
+        return super.open();
+    }
+}
+
+
+
+
+export default new LoginPage();
---
+++ test/specs/test.e2e.js
@@ -0,0 +1,421 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+        
+    })
+})
+
+describe('My Login application', () => {
+    it('should click backpack add to cart', async () => {
+        
+        await LoginPage.addBackpack();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+        
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+        
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(LoginPage.shoppingCart).toBeExisting()
+
+    })
+})
+
+
+describe('My Login application', () => {
+    it('should click the shopping cart icon', async () => {
+        
+        await LoginPage.clickShoppingCart();
+
+        
+    })
+})
+
+describe('Click checkout', () => {
+    it('should click the checkout icon', async () => {
+        await LoginPage.clickCheckOut();
+
+    })
+})
+
+describe('Click Burger Menu', () => {
+    it('should click the burger menu', async () => {
+        await LoginPage.clickHamburgerMenu();
+        await expect(LoginPage.allItems).toBeClickable()
+
+    })
+})
+
+describe('Click Logout', () => {
+    it('should click Logout icon', async () => {
+        await LoginPage.clickLogOut();
+
+    })
+})
